cb595e58bec52f47e7844901cab9d55616dae56e
MusobekErgashev <musobeccoder007@gmail.com>
2026-02-15 11:25:08 +0500

    Add new assets and implement menu with dynamic icons and active state

--- src/app/pages-export.js
+++ src/app/pages-export.js
@@ -1,32 +1,37 @@
 export const Pages = [
     {
         pageName: "Bosh sahifa",
+        iconActive: "/assets/homeActive.png",
+        icon: "/assets/home.png",
         path: "/",
         id: 1,
     },
     {
         pageName: "Video darsliklar",
+        iconActive: "/assets/lessonsActive.png",
+        icon: "/assets/lessons.png",
         path: "/lessons",
         id: 2,
     },
     {
         pageName: "Kutubxona",
+        iconActive: "/assets/libraryActive.png",
+        icon: "/assets/library.png",
         path: "/library",
         id: 3,
     },
     {
         pageName: "Testlar",
+        iconActive: "/assets/quizActive.png",
+        icon: "/assets/quiz.png",
         path: "/quiz",
         id: 4,
     },
-    // {
-    //     pageName: "Bosh sahifa",
-    //     path: "/",
-    //     id: 4,
-    // },
-    // {
-    //     pageName: "Bosh sahifa",
-    //     path: "/",
-    //     id: 1,
-    // },
+    {
+        pageName: "Peshqadamlar",
+        iconActive: "/assets/liderboardActive.png",
+        icon: "/assets/liderboard.png",
+        path: "/liderboard",
+        id: 5,
+    },
 ]
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,28 +3,55 @@
 import Link from 'next/link'
 import React, { useState } from 'react'
 import { Pages } from '@/app/pages-export'
+import { usePathname } from 'next/navigation'
+import Image from 'next/image'
 
 const Menu = () => {
-  const [linkId, setLinkId] = useState(null)
+    const pathname = usePathname()
 
-  return (
-    <div className='bg-white w-80 px-10 flex flex-col justify-between py-5 border-r border-[#DFE5ED]'>
-        <div className='flex flex-col gap-1'>
-            {
-                Pages.map((item) => {
-                    return(
-                        <Link className={`py-3 px-5 bg-[#8144FE] rounded-xl text-white font-medium ${}`}
-                            key={item.id}
-                            href={item.path}>
-                            {item.pageName}
-                        </Link>
-                    )
-                })
-            }
+    return (
+        <div className='bg-white w-80 flex flex-col justify-between py-5 border-r border-[#DFE5ED]'>
+            <div className='flex flex-col gap-1 px-5'>
+                {
+                    Pages.map((item) => {
+                        const isActive = pathname === item.path
+
+                        return (
+                            <Link
+                                className={`py-3 px-4 rounded-xl font-medium flex items-center gap-2.5 transition-all ${isActive
+                                    ? 'bg-[#8144FE] text-white shadow-lg shadow-blue-100'
+                                    : 'text-[#45556C] hover:bg-gray-100'
+                                    }`}
+                                key={item.id}
+                                href={item.path}>
+
+                                <Image
+                                    src={isActive ? item.iconActive : item.icon}
+                                    alt={item.pageName}
+                                    width={24}
+                                    height={24}
+                                    className='w-6 h-6'
+                                />
+
+                                {item.pageName}
+                            </Link>
+                        )
+                    })
+                }
+            </div>
+
+            <div className='pt-5 border-t border-[#DFE5ED] px-5'>
+                <Link href={'/profile'} className={`px-4 py-3 items-center flex rounded-xl transition-all gap-4 ${pathname === "/profile" ? 'bg-[#8144FE] shadow-lg shadow-blue-100' : 'bg-[#F3F7FA] hover:bg-gray-200'}`}>
+                    {/* <Image src="" /> */}
+                    <div className={`w-12 h-12 rounded-full ${pathname === "/profile" ? 'bg-white' : 'bg-[#8144FE]'}`}></div>
+                    <div className={`flex flex-col gap-4.5 ${pathname === "/profile" ? 'text-white' : ''}`}>
+                        <h3 className='font-medium leading-0'>Mrs. Smith</h3>
+                        <p className='font-light text-[14px] leading-0'>Yangi</p>
+                    </div>
+                </Link>
+            </div>
         </div>
-        <div></div>
-    </div>
-  )
+    )
 }
 
 export default Menu
